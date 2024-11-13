1319e13076a2e140c522ad99b98ccb8dc4572ab3
RamathPerera <118724044+RamathPerera@users.noreply.github.com>
2024-11-13 23:00:45 +0530

    Project initialization

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import AllRoute from './routes/allRoute';
+
+const App = () => {
+  return (
+    <div className="app">
+      <AllRoute />
+    </div>
+  );
+};
+
+export default App;
